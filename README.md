ccac7f779ad0759774087526f87ad6e4fdc35aa5
poorthuis-w <wessel.poorthuis@pbl.nl>
2023-05-01 11:29:04 +0200

    Toevoeging script

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Vivet_ref_warmte_woningen
-Python script voor het maken van gemeentebestanden uit het project Vivet referentieverbuik warmte woningen zonder de excel template te gebruiken.
+# Referentieverbruik warmte woningen python
+
+Referentieverbruik warmte woningen python can worden gebruikt om de gemeentebestanden uit het project [Referentieverbuik warmte woningen](https://www.pbl.nl/publicaties/referentieverbruik-warmte-woningen#:~:text=Het%20referentieverbruik%20is%20bedoeld%20voor,voor%20particuliere%20eigenaren%20en%20bewoners.) te genereren zonder daarvoor de excel template te gebruiken. 
+
+## Installatie
+
+Clone via github.
+
+## Usage
+
+Voeg in de map 'gemeente_inputdata' de databestanden voor de gemeenten toe. Deze zijn te vinden in het [PBL dataportaal referentieverbuik warmte woningen](https://dataportaal.pbl.nl/downloads/VIVET/Referentieverbruik_warmte/). 
+
+In het bestand 'rekenregels.py' is met de variabele 'GM_codes' aan te passen welke gemeente(n) meegenomen worden. Door 'alle_inputdata' in te vullen worden alle databestanden in de map 'gemeente_inputdata' gebruikt. 
+
+## License
+
+[GPL3.0](https://choosealicense.com/licenses/gpl-3.0/)
---
+++ outputvariabelen.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 13:43:48 2023
+
+@author: poorthuisw
+"""
+
+output_variabelen_dict = {
+                     'Woning' : {
+                         'vbo_id' : 'code',
+                         },
+                     'Adres'  : {
+                         'Postcode_huisnummer' :'code',
+                          },
+                     'Aantal bewoners' : {
+                          'Aantal bewoners' : 'aantal',
+                          },
+                     'Regio' : {
+                          'gemeente' : 'code',
+                          'wijk'     : 'code',
+                          'buurtcode': 'code',
+                          },
+                     'Woningkenmerken' : {
+                          'Kenmerken' : 'code',
+                          'woningtype': 'code',
+                          'bouwperiode': 'code',
+                          'bouwjaar' : 'jaar',
+                          'schillabel': 'code',
+                          'labeldatum' : 'jaar_maand_dag',
+                          'eigendom' : 'code',
+                          'oppervlakte' : 'm2',
+                          },
+                     'Functionele vraag' : {
+                          'koken' : 'GJ',
+                          'warm tapwater' : 'GJ',
+                          'ruimteverwarming' : 'GJ',
+                          'Totaal' : 'GJ',
+                          'Predictieinterval onderwaarde' : 'GJ',
+                          'Predictieinterval bovenwaarde' : 'GJ',
+                          'Lokale praktijkfactor' : 'factor',
+                          },
+                     'Installatietype' : {
+                         'installatiecode' : 'code',
+                         'Warm tapwater aandeel basis' : 'aandeel',
+                         'Ruimteverwarming aandeel basis' : 'aandeel',
+                         },
+                     'Installatie-efficientie' : {
+                         'koken' : 'factor',
+                         'warm tapwater basis' : 'factor',
+                         'warm tapwater piek' : 'factor',
+                         'ruimteverwarming basis' : 'factor',
+                         'ruimteverwarming piek' : 'factor',
+                         },
+                     'Metervraag aardgas' :{
+                         'koken' : 'GJ',
+                         'warm tapwater basis' : 'GJ',
+                         'warm tapwater piek' : 'GJ',
+                         'ruimteverwarming basis' : 'GJ',
+                         'ruimteverwarming piek' : 'GJ',
+                         },
+                     'Metervraag elektriciteit' :{
+                         'koken' : 'GJ',
+                         'warm tapwater basis' : 'GJ',
+                         'warm tapwater piek' : 'GJ',
+                         'ruimteverwarming basis' : 'GJ',
+                         'ruimteverwarming piek' : 'GJ',
+                         'hulpvraag' : 'GJ'
+                         },
+                     'Metervraag warmtenet' :{
+                         'warm tapwater basis' : 'GJ',
+                         'warm tapwater piek' : 'GJ',
+                         'ruimteverwarming basis' : 'GJ',
+                         'ruimteverwarming piek' : 'GJ',
+                         },
+                     'Metervraag waterstof' :{
+                         'warm tapwater basis' : 'GJ',
+                         'warm tapwater piek' : 'GJ',
+                         'ruimteverwarming basis' : 'GJ',
+                         'ruimteverwarming piek' : 'GJ',
+                         },
+                     'Metervraag biomassa' :{
+                         'warm tapwater basis' : 'GJ',
+                         'warm tapwater piek' : 'GJ',
+                         'ruimteverwarming basis' : 'GJ',
+                         'ruimteverwarming piek' : 'GJ',
+                         },
+                     'Metervraag olie' :{
+                         'ruimteverwarming basis' : 'GJ',
+                         'ruimteverwarming piek' : 'GJ',
+                         },
+                     'Metervraag totaal' : {
+                         'aardgas' : 'GJ',
+                         'elektriciteit' : 'GJ',
+                         'warmtenet' : 'GJ',
+                         'waterstof' : 'GJ',
+                         'biomassa' : 'GJ',
+                         'olie' : 'GJ',
+                         'totaal' : 'GJ',
+                         },
+                     'Regionale klimaatcorrectie' : {
+                         'regionale klimaatcorrectie' : 'factor',
+                         }
+                     }
